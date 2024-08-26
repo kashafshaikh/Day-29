8f48cbfe91b0056d03c40d05db5414cd876bbb58
Kashaf Shaikh <kashaf@example.com>
2024-08-27 02:34:58 +0500

    push my day29 to github

---
+++ day29.ts
@@ -0,0 +1,87 @@
+
+// Day 29
+
+// *Question 1: Calculate the Product of Non-Zero Digits*
+
+// Description:
+
+// Write a TypeScript function that takes a number as input and returns the product of all non-zero digits in the number.
+
+// Example:
+// typescript
+// Input: 4056
+// Output: 120  // 4 * 5 * 6 = 120
+
+// Input: 1002
+// Output: 2  // 1 * 2 = 2
+
+
+// Hint: Loop through each digit, skip zeros, and multiply the rest.
+
+// ---
+
+function productOfNonZeroes(num:number):number{
+    let product = 1;
+    while(num > 0){
+        let digit = num % 10;
+        if(digit != 0){
+          product *= digit;
+        }
+        num = Math.floor(num / 10);
+    }
+    return product;
+}
+console.log(productOfNonZeroes(4056)); // Output: 120
+console.log(productOfNonZeroes(1002)); // Output: 2
+
+// *Question 2: Find the Difference Between the Largest and Smallest Digit*
+
+// Description:
+
+// Write a TypeScript function that takes a number as input and returns the difference between its largest and smallest digits.
+
+// Example:
+// typescript
+// Input: 7593
+// Output: 6  // 9 - 3 = 6
+
+// Input: 12345
+// Output: 4  // 5 - 1 = 4
+
+
+// Hint: Convert the number to a string or use a loop to identify the largest and smallest digits, then calculate their difference.
+
+function findDigitDifference(num: number):number{
+    const numStr = num.toString();
+    let maxDigit = parseInt(numStr[0]);
+    let minDigit = parseInt(numStr[0]);
+
+    for(let i=1; i<numStr.length; i++){
+        let digit = parseInt(numStr[i]);
+        if(digit > maxDigit){
+            maxDigit = digit;
+        }
+        if(digit < minDigit){
+            minDigit = digit
+        }
+    }
+    return maxDigit - minDigit
+}
+console.log(findDigitDifference(7593));  // Output: 6
+console.log(findDigitDifference(12345)); // Output: 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
